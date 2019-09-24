979a220eca236b5c04f688295a4add4ab8e73b72
MateuszR <mateusz.rostkowsky995@gmail.com>
2019-09-24 12:14:54 +0200

    foreach method added

---
+++ js/arrays.js
@@ -0,0 +1,27 @@
+ // First example
+ const paragraph = document.querySelector('#sample-1')
+ var cars = ["Audi", "BMW", "Mercedes", "Volvo"]
+ cars.forEach(displayCars)
+
+ function displayCars(item, index) {
+     paragraph.innerHTML += index + ": " + item + "<br>"
+ }
+
+ // Second example
+ const links = document.querySelectorAll('.ForEachSample a')
+ 
+ function changeToBlack() {
+     links.forEach((links) => {
+         links.style.color = "black";
+     })
+ }
+
+ links.forEach((links, index) => {
+     links.addEventListener('click', function() {
+         console.log("this: " + this)
+         console.log("index: " + index)
+         changeToBlack()
+         this.style.color = "blue"
+     })
+ })
+
